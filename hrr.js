6b4ce13c5ed1bb33d4f8fcdb55afeb2a2188babb
varitoxx <akillerboy@gmail.com>
2019-02-14 21:32:30 +0100

    Añadidos logs

--- hrr.js
+++ hrr.js
@@ -1,4 +1,11 @@
+"use stric";
+
 const express = require("express");
+
+const FileStreamRotator = require('file-stream-rotator');
+const morgan = require('morgan');
+const fs = require('fs');
+
 const bodyParser  = require("body-parser");
 const methodOverride = require("method-override");
 const mongoose = require('mongoose');
@@ -11,6 +18,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(helmet());
+
+var logDirectory = 'log';
+// Se asegura de que el directorio existe
+if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
+}
+// Crea un stream de escritura rotatorio
+var accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: logDirectory + '/access-%DATE%.log',
+    frequency: 'daily',
+    verbose: false
+});
+
+app.use(morgan('combined', { stream: accessLogStream }));
 
 var router = express.Router();
 
