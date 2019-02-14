02ca1f6648aa8b9bee663b55ea3bf3e9713f2ffb
varitoxx <akillerboy@gmail.com>
2019-02-14 21:29:10 +0100

    Añadidas dependencias y control de errores

--- hrr.js
+++ hrr.js
@@ -1,12 +1,16 @@
 const express = require("express");
-const app = express();
 const bodyParser  = require("body-parser");
 const methodOverride = require("method-override");
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+
+var app = express();
+var port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+app.use(helmet());
 
 var router = express.Router();
 
@@ -16,6 +20,10 @@ router.get('/', function(req, res) {
 
 app.use(router);
 
-app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+app.listen(port, function() {
+  console.log("Node server running on http://localhost:%s",port);
+});
+
+process.on('uncaughtException', function (e) {
+  console.error(e);
 });
