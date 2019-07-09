3e1796bd47df663e699cb75332eeebf32199ce4a
varitoxx <akillerboy@gmail.com>
2019-07-09 14:58:00 +0200

    Añadido test activate si no hay usuario que activar

--- test/activate.js
+++ test/activate.js
@@ -11,18 +11,6 @@ const url = 'http://localhost:3000';
 
 describe("Activation OK: ", () => {
     var userCreated = null;
-    beforeEach(() => {
-        // var userToSend = {
-        //     email: Math.random().toString(36).substring(7) + "@localhost.com",
-        //     password: Math.random().toString(36).substring(7),
-        // }
-        // chai.request(url)
-        //     .post("/register")
-        //     .send(userToSend)
-        //     .end(function (err, res) {
-        //         userCreated = userToSend;
-        //     });
-    });
     it("Should activate a user", (done) => {
         var userToSend = {
             email: Math.random().toString(36).substring(7) + "@localhost.com",
@@ -33,6 +21,7 @@ describe("Activation OK: ", () => {
             .send(userToSend)
             .end(function (err, res) {
                 userCreated = res.body.user;
+                console.log(userCreated)
                 registerToken.generate(userCreated, function (err, token) {
                     chai.request(url)
                         .get("/activate")
@@ -46,10 +35,34 @@ describe("Activation OK: ", () => {
                 });
             })
     });
-    afterEach(() => {
-        users.remove(userCreated.access_token, function (err) {})
+    after((done) => {
+        users.remove(userCreated.access_token, function (err) {
+            done()
+        })
     });
 });
-// Activación normal
+
+describe("Activation Fails: ", () => {
+    var userCreated = null;
+    it("It should fail if there is no user to activate", (done) => {
+        var fakeUser = {
+            email: "fake" + Math.random().toString(36).substring(7) + "@localhost.com"
+        }
+        fakeUser.access_token = users.getToken(fakeUser.email, Math.random().toString(36).substring(7));
+
+        registerToken.generate(fakeUser, function (err, token) {
+            chai.request(url)
+                .get("/activate")
+                .query({
+                    id: token
+                })
+                .end(function (err, res) {
+                    console.error(res.body)
+                    expect(res).to.have.status(403);
+                    done();
+                })
+        });
+    });
+});
 // Activación sin usuario
 // Activación sin ticket
